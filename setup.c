001eaa78181f9357a536dead1270f48823fd964e
Martell94 <116161025+Martell94@users.noreply.github.com>
2022-12-20 09:56:30 +0100

    Update setup.c

--- setup.c
+++ setup.c
@@ -1,23 +1,22 @@
 #include "header.h"
 
-led_t led1, led2, led3, led4, led5; //Här deklareras dessa struktar för bruk.
-static inline void init_interrupts();
+led_t led1, led2, led3, led4, led5; //HÃ¤r deklareras dessa struktar fÃ¶r bruk.
 
 /********************************************************************************
 							*void setup();
-Denna rutin initierar våra deklarerade led_struktar, samt ettställer div. portar.
+Denna rutin initierar vÃ¥ra deklarerade led_struktar, samt ettstÃ¤ller div. portar.
 ********************************************************************************/
 void setup(){
 		
-	led_init (&led1, 6); //Skickar vår ledstrukt samt dess pinnr till led_init.
+	led_init (&led1, 6); //Skickar vÃ¥r ledstrukt samt dess pinnr till led_init.
 	led_init (&led2, 7); //Etc, etc...
 	led_init (&led3, 8);
 	led_init (&led4, 9);
 	led_init (&led5, 10);
 	
 	PORTD = (1 << BUTTON1) | (1 << BUTTON2); 
-	//^Här skippar vi att använda vårt set-makro, då vi inte kan ettställa flera samtidigt med det. 
-	PORTB = (1<<BUTTON3) | (1<<BUTTON4) | (1<<BUTTON5); //Samma här.
+	//^HÃ¤r skippar vi att anvÃ¤nda vÃ¥rt set-makro, dÃ¥ vi inte kan ettstÃ¤lla flera samtidigt med det. 
+	PORTB = (1<<BUTTON3) | (1<<BUTTON4) | (1<<BUTTON5); //Samma hÃ¤r.
  
 	init_interrupts(); //Hoppar till denna funktion.
 	return;
@@ -25,21 +24,18 @@ void setup(){
 
 /********************************************************************************
 							*init_interrupts();
-		Initierar våra interrupts, både i PCI samt EICRA-registret. 
-		Denna har typen inline, vilket innebär att när den kallas på,
-		så klistras den in i den funktion den kallas från istället för
-		att vi hoppar hit. Detta sparar några moment i programexekverigen.
+		Initierar vÃ¥ra interrupts, bÃ¥de i PCI samt EICRA-registret. 
 ********************************************************************************/
-static inline void init_interrupts(){
+ void init_interrupts(){
 	
-	asm("SEI"); //Assemblerinstruktion för att initierar nterrupts globalt.
-	EICRA |= ((1<<ISC01) | (1<< ISC00) | (1<<ISC11) | (1<< ISC10)); //Här initierar vi vår avbrottsvektor.
+	asm("SEI"); //Assemblerinstruktion fÃ¶r att initierar nterrupts globalt.
+	EICRA |= ((1<<ISC01) | (1<< ISC00) | (1<<ISC11) | (1<< ISC10)); //HÃ¤r initierar vi vÃ¥r avbrottsvektor.
 	
 	
 	EIMSK |= (1<<INT0) |  (1<<INT1);
-	/*^Initierar interrupts på stigande flank.*/
+	/*^Initierar interrupts pÃ¥ stigande flank.*/
 	
 	PCICR |= (1<<PCIE0); //Initierar interrupts via PCI-registret. 
-	PCMSK0 |= (1 << BUTTON3) | (1 << BUTTON4) | (1 << BUTTON5); //Initierar interrupts på specifikt knapp-pinsen.
+	PCMSK0 |= (1 << BUTTON3) | (1 << BUTTON4) | (1 << BUTTON5); //Initierar interrupts pÃ¥ specifikt knapp-pinsen.
 	return;
 }
